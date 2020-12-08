fad956ed0868bb94ac208042cdba3d841560fdcf
li-libo <2414493661@qq.com>
2020-12-08 15:42:37 +0800

    伪服务挡板

--- src/main/java/cn/com/rest/AbsController.java
+++ src/main/java/cn/com/rest/AbsController.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +30,12 @@ public class AbsController {
 	private static final String SESSION_ID = "SESSION_ID";
 	
 	private static Map<String, Map<String, Object>> sessionResourceMap = new ConcurrentHashMap<>();
+	
+	private static Map<String, Integer> 年薪map = new ConcurrentHashMap<>();
+	
+	private static Map<String, Integer> 资金map = new ConcurrentHashMap<>();
+	
+	private static Map<String, Integer> 年限map = new ConcurrentHashMap<>();
 
 	static {
 		Map<String, Object> resourceMap = new HashMap<>();
@@ -37,13 +44,25 @@ public class AbsController {
 		resourceMap.put("证件/证件信息:客户号", 0);
 		resourceMap.put("介质/介质信息:介质类型", 9);
 		resourceMap.put("证件/证件信息:合法结果", 1);
-		resourceMap.put("证件/选择证件类型:证件类型", 2);
+		resourceMap.put("证件/选择证件类型:证件类型", "2");
+		//证件/证件信息:证件类型
+		resourceMap.put("证件/证件信息:证件类型", 2);
 		resourceMap.put("证件/证件选择:证件类型", 1);
 		sessionResourceMap.put("1", resourceMap);		
 		// 准备adpsAddress地址
 		for(int port = 8800; port< 8900; port++) {
 			adpsAddressSourceQueue.offer("localhost:" + port);
 		}
+		年薪map.put("小胖子", 30000);
+		年薪map.put("懂王", 100000);
+		年薪map.put("马大帅", 10000);
+		资金map.put("小胖子", 20000);
+		资金map.put("懂王", 10000000);
+		资金map.put("马大帅", 10000);
+		年限map.put("小胖子", 4);
+		年限map.put("懂王", 10);
+		年限map.put("马大帅", 2);
+		
 	}
 
 	@RequestMapping(path = "/clearBrokerAddressesOfRegistration", method = RequestMethod.GET)
@@ -95,5 +114,27 @@ public class AbsController {
 		}
 		return resultMap;
 	}
+	
+	@RequestMapping(path = "/获取资金", method = RequestMethod.GET)
+	public String 获取资金(@RequestParam(name = "姓名") String 姓名) throws JSONException{
+		JSONObject result = new JSONObject().put("资金", 资金map.get(姓名));
+		return result.toString();
+	}
+	
+	@RequestMapping(path = "/获取年限", method = RequestMethod.GET)
+	public String 获取年限(@RequestParam(name = "姓名") String 年限) throws JSONException{
+		JSONObject result = new JSONObject().put("年限", 年限map.get(年限));
+		return result.toString();
+	}
+	
+	@RequestMapping(path = "/获取年薪", method = RequestMethod.GET)
+	public String 获取年薪(@RequestParam(name = "姓名") String 姓名) throws JSONException{
+		JSONObject result = new JSONObject().put("年薪", 年限map.get(姓名));
+		return result.toString();
+	}
 
+	@RequestMapping(path = "/接收通知", method = RequestMethod.PUT)
+	public void 接收通知(@RequestBody String requestBody) throws JSONException{
+		System.out.println(requestBody);
+	}
 }
